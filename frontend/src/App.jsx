765389569abe106ef9a668c314bd630b6101a090
Rawand A.Ali <rawand.abubakir@gmail.com>
2023-05-27 21:09:58 +0300

    create todo module

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import Nav from './components/nav'
 import Hero from './pages/hero'
+import Todos from './pages/todos'
 function App() {
 
 
   return (
     <div>
       <Nav/>
-      <Hero/>
+      <Todos/>
     </div>
   )
 }
