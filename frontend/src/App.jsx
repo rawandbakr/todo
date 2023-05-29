d032d78aa48d0dbb5ebb30b570925930eb1105d7
Rawand A.Ali <rawand.abubakir@gmail.com>
2023-05-29 15:37:05 +0000

    Update 2 files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,17 @@
-import Nav from './components/Nav'
-import Hero from './pages/hero'
-import Todos from './pages/todos'
+import Nav from "./components/Nav";
+import Todos from "./pages/todos";
 function App() {
-
-
   return (
     <div>
-      <Nav/>
-      <Todos data={"Enim coma non. "}/>
-      <Todos data={"Enim commodo velit veniam nisi magna non.EEnim commodo velit veniam nisi magna non.Enim commodo velit veniam nisi magna non.Enim commodo velit veniam nisi magna non.Enim commodo velit veniam nisi magna non.nim commodo velit veniam nisi magna non.  qui nostrud. Sit enim nulla nostrud im Lorem enim ex."}/>
-
+      <Nav />
+      <Todos data={"Enim coma non. "} />
+      <Todos
+        data={
+          "Enim commodo velit veniam nisi magna non.EEnim commodo velit veniam nisi magna non.Enim commodo velit veniam nisi magna non.Enim commodo velit veniam nisi magna non.Enim commodo velit veniam nisi magna non.nim commodo velit veniam nisi magna non.  qui nostrud. Sit enim nulla nostrud im Lorem enim ex."
+        }
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
